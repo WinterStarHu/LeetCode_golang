e65b3921ace3adfd3a172aad74e7e390a1e4cdb3
WinterStar <hdxshr@163.com>
2021-01-31 21:11:00 +0800

    优化:0ms 3MB

--- towSum.go
+++ towSum.go
@@ -3,20 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	var nums =[]int{3,2,4}
+	var nums = []int{3, 2, 4}
 	target := 6
-	fmt.Println(twoSum(nums,target))
+	fmt.Println(twoSum(nums, target))
 }
 func twoSum(nums []int, target int) []int {
-	var dMap = make(map[int]int)
+	dMap := make(map[int]int)
 	for idx, val := range nums {
-		_, ok := dMap[target-val]
-		if ok {
-			return []int{idx, dMap[target-val]}
+		minus := target - val
+		if _, ok := dMap[minus]; ok {
+			return []int{dMap[minus], idx}
 		} else {
 			dMap[val] = idx
 		}
 	}
 	return nil
 }
-
